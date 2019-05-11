2e503d683e6649e9e7111e748ee0605b3365de27
Jose Tavares <josetavares00@gmail.com>
2019-05-11 13:24:08 +0100

    remove gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,14 +5,7 @@ module.exports = {
     author: ``,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
+    `gatsby-plugin-react-helmet`,    
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
