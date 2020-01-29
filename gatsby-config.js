ab96e9b2522f3bdf9961340b8a2c71343f2873ba
Jose Tavares <josetavares00@gmail.com>
2020-01-29 19:17:36 +0000

    use emotion

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,6 +76,7 @@ module.exports = {
             }
         },
         `gatsby-plugin-netlify`,
-        `gatsby-plugin-offline`
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-emotion`
     ]
 };
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,30 +1,91 @@
 import React from "react";
 import { Link } from "gatsby";
+import styled from "@emotion/styled";
 import logo from "../../images/logo.svg";
-import "./header.less";
+
+const HeaderComponent = styled.header`
+    display: flex;
+    max-width: 1024px;
+    height: 100px;
+    padding: 0 30px;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 auto;
+
+    @media screen and (max-width: 1023px) {
+        height: 60px;
+        padding: 0 20px;
+    }
+`;
+
+const LogoContainer = styled.div`
+    width: 40px;
+    height: 40px;
+    &:hover {
+        opacity: 0.8;
+    }
+`;
+
+const Menus = styled.div`
+    display: flex;
+`;
+
+const StyledLink = styled(Link)`
+    color: #222222;
+    text-decoration: none;
+    padding: 10px 5px;
+    border-left: 5px solid transparent;
+    font-size: 15px;
+    position: relative;
+    overflow: hidden;
+
+    &:before {
+        content: "";
+        position: absolute;
+        z-index: -1;
+        left: 51%;
+        right: 51%;
+        bottom: 0;
+        background: #0f7c79;
+        height: 2px;
+        transition-property: "left, right";
+        transition-duration: 0.3s;
+        transition-timing-function: ease-out;
+    }
+
+    &:hover,
+    &:focus,
+    &:active,
+    &.active {
+        &:before {
+            left: 0;
+            right: 0;
+        }
+    }
+`;
 
 const Header = () => (
-    <header>
-        <div className="logo">
+    <HeaderComponent>
+        <LogoContainer>
             <Link to="/">
                 <img src={logo} alt="Logo" />
             </Link>
-        </div>
+        </LogoContainer>
 
-        <div className="menus">
-            <Link to="/" className="link" activeClassName="active">
+        <Menus>
+            <StyledLink to="/" activeClassName="active">
                 Hello
-            </Link>
+            </StyledLink>
 
-            <Link to="/portfolio/" className="link" activeClassName="active">
+            <StyledLink to="/portfolio/" activeClassName="active">
                 Portfolio
-            </Link>
+            </StyledLink>
 
-            <Link to="/about/" className="link" activeClassName="active">
+            <StyledLink to="/about/" activeClassName="active">
                 About
-            </Link>
-        </div>
-    </header>
+            </StyledLink>
+        </Menus>
+    </HeaderComponent>
 );
 
 export default Header;
