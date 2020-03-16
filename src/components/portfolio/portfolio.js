d6a046051d3415180d71b21d689378fc339b95ca
Jose Tavares <josetavares00@gmail.com>
2020-03-16 21:30:55 +0000

    remove unused css

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,17 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "@emotion/styled";
 import Project from "../project/project";
-import "./portfolio.less";
+
+const Title = styled.h1`
+    max-width: 1024px;
+    margin: 40px auto;
+
+    @media screen and (max-width: 1023px) {
+        padding: 0 20px;
+    }
+`;
+
+const ProjectsList = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+    margin-bottom: 20px;
+
+    @media screen and (max-width: 500px) {
+        grid-template-columns: 1fr;
+    }
+`;
 
 const Portfolio = ({ posts, showTitle }) => {
     return (
         <div className="portfolio">
-            {showTitle && <h1>Portfolio</h1>}
-            <div className="projects-list">
+            {showTitle && <Title>Portfolio</Title>}
+            <ProjectsList>
                 {posts.edges.map(({ node }) => (
                     <Project node={node} key={node.fields.slug} />
                 ))}
-            </div>
+            </ProjectsList>
         </div>
     );
 };
