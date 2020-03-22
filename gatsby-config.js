79fdd0b001e2ce3df7559ba136f340ec04c78d13
Jose Tavares <josetavares00@gmail.com>
2020-03-22 16:17:04 +0000

    update deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,80 +1,80 @@
 module.exports = {
-    siteMetadata: {
-        title: `Juliana Reis`,
-        description: `Juliana Reis Personal Website`,
-        author: ``,
-        siteUrl: `https://www.julianareis.me`
+  siteMetadata: {
+    title: `Juliana Reis`,
+    description: `Juliana Reis Personal Website`,
+    author: ``,
+    siteUrl: `https://www.julianareis.me`,
+  },
+  plugins: [
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-robots-txt`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Juliana Reis`,
+        short_name: `Juliana Reis`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#0f7c79`,
+        display: `fullscreen`,
+        icon: `src/images/logo.svg`,
+      },
     },
-    plugins: [
-        `gatsby-plugin-sitemap`,
-        `gatsby-plugin-react-helmet`,
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        `gatsby-plugin-robots-txt`,
-        {
-            resolve: `gatsby-plugin-manifest`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [{ family: `Nunito` }, { family: `Montserrat` }],
+        display: "swap",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GA,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-video",
             options: {
-                name: `Juliana Reis`,
-                short_name: `Juliana Reis`,
-                start_url: `/`,
-                background_color: `#ffffff`,
-                theme_color: `#0f7c79`,
-                display: `fullscreen`,
-                icon: `src/images/logo.svg`
-            }
-        },
-        {
-            resolve: `gatsby-plugin-prefetch-google-fonts`,
+              width: "auto",
+              height: "auto",
+              preload: "auto",
+              muted: true,
+              autoplay: true,
+              playsinline: true,
+              controls: true,
+              loop: true,
+            },
+          },
+          {
+            resolve: `gatsby-remark-images`,
             options: {
-                fonts: [{ family: `Nunito` }, {family: `Montserrat`}],
-                display: "swap"
-            }
-        },
-        {
-            resolve: `gatsby-plugin-google-analytics`,
-            options: {
-                trackingId: process.env.GA
-            }
-        },
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [
-                    {
-                        resolve: 'gatsby-remark-video',
-                        options: {
-                            width: 'auto',
-                            height: 'auto',
-                            preload: 'auto',
-                            muted: true,
-                            autoplay: true,
-                            playsinline: true,
-                            controls: true,
-                            loop: true
-                        }
-                    },
-                    {
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 1024,
-                            quality: 100,
-                            withWebp: true,
-                            linkImagesToOriginal: false
-                        }
-                    },
-                    `gatsby-remark-copy-linked-files`,
-                    `gatsby-remark-containers`
-                ]
-            }
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/content/blog`,
-                name: "blog"
-            }
-        },
-        `gatsby-plugin-netlify`,
-        `gatsby-plugin-emotion`
-    ]
+              maxWidth: 1024,
+              quality: 100,
+              withWebp: true,
+              linkImagesToOriginal: false,
+            },
+          },
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-containers`,
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: "blog",
+      },
+    },
+    `gatsby-plugin-netlify`,
+    `gatsby-plugin-emotion`,
+  ],
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -8,66 +8,69 @@ const path = require("path");
 
 const { createFilePath, createFileNode } = require(`gatsby-source-filesystem`);
 exports.createPages = ({ actions, graphql }) => {
-    const { createPage } = actions;
-    const blogPostTemplate = path.resolve(`src/components/blog-post/blog-post.js`);
-    return new Promise((resolve, reject) => {
-        resolve(
-            graphql(`
-                {
-                    allMarkdownRemark(
-                        sort: { order: DESC, fields: [frontmatter___date] }
-                        limit: 1000
-                    ) {
-                        edges {
-                            node {
-                                fields {
-                                    slug
-                                }
-                                frontmatter {
-                                    title
-                                }
-                            }
-                        }
-                    }
+  const { createPage } = actions;
+  const blogPostTemplate = path.resolve(
+    `src/components/blog-post/blog-post.js`
+  );
+  return new Promise((resolve, reject) => {
+    resolve(
+      graphql(`
+        {
+          allMarkdownRemark(
+            sort: { order: DESC, fields: [frontmatter___date] }
+            limit: 1000
+          ) {
+            edges {
+              node {
+                fields {
+                  slug
                 }
-            `).then(result => {
-                if (result.errors) {
-                    console.log(result.errors);
-                    return reject(result.errors);
+                frontmatter {
+                  title
                 }
-                const blogTemplate = path.resolve(
-                    "./src/components/blog-post/blog-post.js"
-                );
-
-                // Create blog posts pages.
-                const posts = result.data.allMarkdownRemark.edges;
-
-                posts.forEach((post, index) => {
-                    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-                    const next = index === 0 ? null : posts[index - 1].node
-                    createPage({
-                        path: post.node.fields.slug,
-                        component: blogTemplate,
-                        context: {
-                            slug: post.node.fields.slug,
-                            previous,
-                            next,
-                        } // additional data can be passed via context
-                    });
-                });
-            })
+              }
+            }
+          }
+        }
+      `).then((result) => {
+        if (result.errors) {
+          console.log(result.errors);
+          return reject(result.errors);
+        }
+        const blogTemplate = path.resolve(
+          "./src/components/blog-post/blog-post.js"
         );
-    });
+
+        // Create blog posts pages.
+        const posts = result.data.allMarkdownRemark.edges;
+
+        posts.forEach((post, index) => {
+          const previous =
+            index === posts.length - 1 ? null : posts[index + 1].node;
+          const next = index === 0 ? null : posts[index - 1].node;
+          createPage({
+            path: post.node.fields.slug,
+            component: blogTemplate,
+            context: {
+              slug: post.node.fields.slug,
+              previous,
+              next,
+            }, // additional data can be passed via context
+          });
+        });
+      })
+    );
+  });
 };
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-    const { createNodeField } = actions;
-    if (node.internal.type === `MarkdownRemark`) {
-        const slug = createFilePath({ node, getNode, basePath: `pages` });
-        createNodeField({
-            node,
-            name: `slug`,
-            value: slug
-        });
-    }
+  const { createNodeField } = actions;
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` });
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    });
+  }
 };
